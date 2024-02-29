283ef9d2c5dabd95e87da45689b10301cb48db79
Amal Nasr <nasr.amal88@gmail.com>
2024-02-29 22:45:55 +0100

    add dropdown ingrédients & css cards

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,13 +1,31 @@
 import { dropdownTemplate } from "../templates/dropdown.js"
-import {RecipeCard} from "../templates/RecipeCard.js"
+import {RecipeCard} from "../templates/RecipeCard.js" 
 import {recipes} from "../recipes.js"
 class home{
     constructor(){
-        new dropdownTemplate(1,["test1","test2"]);
-        new dropdownTemplate(2,["test3","test4"]);
-        new RecipeCard(recipes[0]).createCard();
+        this.displayDropDown();
+        this.displayRecipes();
     }
-     
-
+    
+    displayDropDown(){
+        
+        const ingredients = [...new Set(recipes.flatMap(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient)))];
+    
+        
+    
+        // // Récupérez le conteneur où vous voulez afficher le dropdown
+         const dropdownContainer = document.getElementById("filters");
+    
+        // // Créez et ajoutez le dropdown d'ingrédients
+        
+         const dropdown = new dropdownTemplate("Ingrédients", "ingredients-dropdown", ingredients);
+       
+    }
+    displayRecipes() {
+        recipes.forEach(recipeData => {
+            new RecipeCard(recipeData).createCard();
+        });
+    }
 }
 new home();
+
--- scripts/templates/dropdown.js
+++ scripts/templates/dropdown.js
@@ -1,5 +1,6 @@
 export class dropdownTemplate {
-  constructor(idDropdown, items) {
+  constructor(nameDropDown,idDropdown, items) {
+    this.NameDropDown = nameDropDown;
     this.IdDropdown = idDropdown;
     this.Items = items;
     this.createTemplate();
@@ -10,15 +11,17 @@ export class dropdownTemplate {
       <div class="relative group ">
         <button id="dropdown-button-${
           this.IdDropdown
-        }" class=" flex inline-flex w-48 h-14 p-3 items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
-          <span class="mr-2  font-manrope font-medium text-base">Open Dropdown</span>
+        }" class=" flex inline-flex w-48 h-14 p-3 items-center justify-center w-full px-4 py-2 text-sm font-medium text-black bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
+          <span class="dropDownName mr-2  font-manrope font-medium text-base">${
+            this.NameDropDown
+          }</span>
           <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M6.293 9.293a1 1 0 011.414 0L10 11.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
           </svg>
         </button>
         <div id="dropdown-menu-${
           this.IdDropdown
-        }" class="hidden absolute text-sm	font-normal right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1">
+        }" class="ddlMenu hidden absolute text-sm	font-normal z-1 right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1 ">
           <!-- Search input -->
           <label class="relative block">
               <input id="search-input-${
@@ -31,12 +34,12 @@ export class dropdownTemplate {
                         d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z">
                     </path>
                 </svg>
-            </span>
-           </label>
+              </span>
+          </label>
 
           <button class="deleteDdlBtn" tabindex="0" style="display: block;"></button>
           <!-- Dropdown content goes here -->
-          <ul>
+          <ul class="h-48 py-2 overflow-y-auto text-gray-700 dark:text-gray-200">
            ${this.Items.map(
              (element, index) =>
                `<li id="${this.IdDropdown}-Item${index}" class="item block px-4 py-2 text-gray-700 hover:bg-customYellow active:bg-blue-100 cursor-pointer rounded-md font-normal text-sm">${element}</li>`
@@ -93,15 +96,15 @@ export class dropdownTemplate {
     items.forEach((item) => {
       item.addEventListener("click", () => {
         const tagWord = `
-        <div class="inline-flex items-center h-8 overflow-hidden text-white bg-blue-500 rounded">
+        <div class="inline-flex items-center overflow-hidden text-white  h-14   px-4 py-2 text-sm font-medium  rounded-md rounded bg-customYellow text-black gap-15 mt-5">
          <span id="tag-${
            item.id
-         }"  class="tagItem inline-flex items-center bg-customYellow px-2 py-1 me-2 text-sm font-medium  rounded text-black">
+         }"  class="tagItem inline-flex items-center bg-customYellow px-2 py-1 me-2 text-sm mr-2  font-manrope font-normal text-base font-medium rounded text-black">
            ${item.textContent.toLowerCase()}
          </span>
        
          <button onclick="return this.parentNode.remove();"
-           class="inline-flex items-center justify-center w-8 h-8 transition-color focus:outline-none focus:ring"
+           class="inline-flex items-center justify-center w-13 h-4 transition-color focus:outline-none focus:ring"
            type="button"
          >
            <span class="sr-only" > Close </span>
